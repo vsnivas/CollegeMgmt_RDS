39abe718ad1595100ba230bb0c7c0c001b4bb571
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-09-07 00:09:23 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'rdsmigrationdb.c6bthdivt8ih.ap-southeast-2.rds.amazonaws.com',
+	'hostname' => 'collegemgmtdb.cgiubsxpc6xe.ap-southeast-1.rds.amazonaws.com',
 	'username' => 'admin',
 	'password' => 'admin123',
 	'database' => 'college_mgmt',
